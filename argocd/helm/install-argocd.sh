1c11138aafbb4d76221eb1ce8bf0e3412d07fd98
alsyean <alsyean@gmail.com>
2024-07-29 23:37:05 +0900

    argocd update

--- argocd/helm/install-argocd.sh
+++ argocd/helm/install-argocd.sh
@@ -7,7 +7,7 @@ kubectl create namespace argocd
 
 # Helm 저장소 추가 및 업데이트
 helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
+# helm repo update
 
 # Argo CD 설치
 helm install argocd argo/argo-cd -n argocd -f argocd/helm/argocd-values.yaml
